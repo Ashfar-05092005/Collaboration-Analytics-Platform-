ced022ce4018109de607707691c67f6467de8994
Ashfar-05092005 <mohammedashfar05092005@gmail.com>
2026-03-25 17:16:17 +0530

    chore: commit current workspace changes

--- backend/scripts/hash_password.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-const password = process.argv[2] || 'ashfar';
-bcrypt.hash(password, 10).then((hash) => {
-  console.log(hash);
-}).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
